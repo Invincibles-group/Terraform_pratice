92df43bb8f4ff05d38ae53bcd99ad8b1deef8c79
Hemant <gorantlahemant@gmail.com>
2023-09-15 12:13:49 -0400

    Day3 understanding the teraform folder structure

---
+++ day3/variables.tf
@@ -0,0 +1,8 @@
+variable "ami_value" {
+    description = "ami value the os will be installed on the Ec2 instance"
+  
+}
+variable "instance_type_value" {
+    description = "Instance type we want "
+  
+}
