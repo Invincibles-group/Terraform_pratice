2ae05aac9e117d287aa88e4c1dd02bfdc25b69ba
Hemant Gorantla <55294714+Invincibles-group@users.noreply.github.com>
2023-09-16 18:36:03 +0000

    moved main.tf

---
+++ day5/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region "us-east-1"
+  
+}
+
+module "ec2_instance" {
+    source = "./day5/modules/ec2_instance"
+    ami_value = "ami-04cb4ca688797756f"
+    instance_type_value = "t2.micro"
+  
+}
+
+module "vpc" {
+
+    source = "./day5/modules/vpc"
+    cidr_block_vpc = "10.0.0.0/16"
+    cidr_block_public_subnet = "10.0.1.0/24"
+    cidr_block_private_subnet = "10.0.2.0/24"
+    cidr_block_public_RT = "0.0.0.0/0"
+  
+}
