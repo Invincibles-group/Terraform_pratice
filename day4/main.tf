7ad5423923030c9093163747aa5485b8d1b9f60b
Hemant <gorantlahemant@gmail.com>
2023-09-16 12:05:50 -0400

    added main.tf

---
+++ day4/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+    source = "./modules/ec2A_instance"
+    ami_value = "ami-04cb4ca688797756f"
+    instance_type_value = "t2.micro"
+    
+}
