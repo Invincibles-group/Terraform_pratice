c10e4c9d57bb483f8e9c0dfe7f38f6e91d6cd631
Hemant <gorantlahemant@gmail.com>
2023-09-16 14:29:28 -0400

    changes in the source path

--- day5/day5/main.tf
+++ day5/day5/main.tf
@@ -12,7 +12,7 @@ module "ec2_instance" {
 
 module "VPC" {
 
-    source = "./day5/modules/VPC"
+    source = "./day5/modules/vpc"
     cidr_block_vpc = "10.0.0.0/16"
     cidr_block_public_subnet = "10.0.1.0/24"
     cidr_block_private_subnet = "10.0.2.0/24"
